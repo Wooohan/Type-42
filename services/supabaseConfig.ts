148ffb5bf96c47f4541c5e09d257974412f7695a
Wooohan <wooohan3@gmail.com>
2026-01-03 06:03:36 +0500

    Update supabaseConfig.ts

--- services/supabaseConfig.ts
+++ services/supabaseConfig.ts
@@ -2,7 +2,7 @@ import { createClient } from '@supabase/supabase-js';
 
 // Supabase configuration
 const supabaseUrl = import.meta.env.VITE_SUPABASE_URL || 'https://fiuodbhgvmylvbanbfve.supabase.co';
-const supabaseAnonKey = import.meta.env.VITE_SUPABASE_ANON_KEY || 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImZpdW9kYmhndm15bHZiYW5iZnZlIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDYyNjIxMzEsImV4cCI6MjA2MTgzODEzMX0.7V_0vJqYb8PcQ2Wz3Z8k8Z8Z8Z8Z8Z8Z8Z8Z8Z8Z8Z8';
+const supabaseAnonKey = import.meta.env.VITE_SUPABASE_ANON_KEY || 'sb_secret_x33xGa8YmioWvfyvDtWNXA_fT_8VL9V';
 
 // Create Supabase client
 export const supabase = createClient(supabaseUrl, supabaseAnonKey, {
