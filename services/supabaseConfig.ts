5bf4a8531633a9f25ec3246bbd0b86d39a7682bb
Wooohan <wooohan3@gmail.com>
2026-01-04 01:36:12 +0500

    Update supabaseConfig.ts

--- services/supabaseConfig.ts
+++ services/supabaseConfig.ts
@@ -2,7 +2,7 @@ import { createClient } from '@supabase/supabase-js';
 
 // Supabase configuration
 const supabaseUrl = import.meta.env.VITE_SUPABASE_URL || 'https://rbdwqdrqnmshebnhpecr.supabase.co';
-const supabaseAnonKey = import.meta.env.VITE_SUPABASE_ANON_KEY || 'sb_publishable_Q2_aDlgKBA6DNzG5eOJndQ_6nr_rq45';
+const supabaseAnonKey = import.meta.env.VITE_SUPABASE_ANON_KEY || 'sb_publishable_SmKN_i3QMQf-yvEfIJrdqw_Gm25hNWG';
 
 // Create Supabase client
 export const supabase = createClient(supabaseUrl, supabaseAnonKey, {
